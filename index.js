859b5755f77ab83b8acdf5c2595da8597c825f82
LeSean Johnson <leseanbiz@gmail.com>
2019-07-26 13:22:02 -0700

    unsorted merge complete

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const abbreviations = require("./data/abbreviations.json");
 const capitals = require("./data/capitals.json");
 const currencies = require("./data/currencies.json");
 
+const merged = abbreviations.map(abb => {
+  const capVal = capitals.filter(cap => {
+    return cap.name === abb.name;
+  });
+  const curVal = currencies.filter(cur => {
+    return cur.name === abb.name;
+  });
+  let output = new Object();
+  output.abbreviation = abb.abbreviation;
+  output.name = abb.name;
+  output.city = capVal[0].city;
+  output.currency_name = curVal[0].currency_name;
+  return output;
+});
+//"can merge all JSOn files"
+console.log(merged.find(v => v.abbreviation === "AF"));
+console.log(merged[0].city);
+
+console.log("merged:", merged);
+
 module.exports = {
   merged: [],
 
   findWhere() {},
-  sort() {},
+  preMergeSort() {},
   updateWhere() {},
 
   abbreviations,
